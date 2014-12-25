fdf306dc5f4d03148f19975a344ae4c9fb22ad61
colindameli0 <colin.damelio@gmail.com>
2014-12-25 12:04:41 -0500

    its xmas, f-off

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,75 +1,78 @@
-$(document).ready(function(){
-
-	// SCROLL-TO-NEXT-PAGE
-	$(".page-down").click(function(e) {
-		event.preventDefault();
-	    $('body').animate({
-	    	scrollTop: $("#about").offset().top}, 700);
-	});
-
-	//SMOOTH SCROLL MENU TOGGLE
-	$('a[href^="#"]').click(function(e) {
-	    var target = $(this.hash);
-	    if (target.length == 0) target = $('a[name="' + this.hash.substr(1) + '"]');
-	    if (target.length == 0) target = $('html');
-	    $('html, body').animate({ scrollTop: target.offset().top -60 }, 500);
-	    return false;
-	});
-
-	//SHOW FIXED NAV ON SCROLL
-	$(window).scroll(function() {    
-	    var scroll = $(window).scrollTop();
-
-	    if (scroll >= 350) {
-	        $(".nav").addClass("switch");
-	        $(".nav").addClass("logo");
-	        $(".nav h3").removeClass("hide");
-	        $(".nav ul").addClass("collapse");
-	        $(".nav ul").addClass("space");
-	    } else {
-	        $(".nav").removeClass("switch");
-	        $(".nav h3").addClass("hide");
-	    } 
-	});
-
-	//RANDOM COLOUR GENERATOR FOR FOOTER SOCIAL ICONS
-	$('.social-footer a').on('mouseover', function(){
-	  $(this).css('color', randomColor());
-	});
-
-	$('.social-footer a').on('mouseout', function(){
-	  $(this).css('color', '#fff');
-	});
 
 
-	//MOBILE MENU DISPLAY
-	$('.mobile-menu').click(function(e){
-		e.preventDefault();
-		$('.nav ul').slideToggle('fast');
-	});
-	
+$(document).ready(function() {
 
-	//HIDE MENU ON LINK CLICK
-	$(".nav li a").on('click', function() {
-		$(".nav-menu").hide();
-		$('.mobile-menu').toggleClass('open');
-	 });
+    // (function($) {
+        // SCROLL-TO-NEXT-PAGE
+        $(".page-down").click(function(e) {
+            event.preventDefault();
+            $('body').animate({
+                scrollTop: $("#about").offset().top
+            }, 700);
+        });
 
-	$(function() {
-		$('.mobile-menu').click(function(e) {
-		  e.preventDefault();
-		  $(this).toggleClass('open');
-		});
-	});
+        //SMOOTH SCROLL MENU TOGGLE
+        $('a[href^="#"]').click(function(e) {
+            var target = $(this.hash);
+            if (target.length === 0) target = $('a[name="' + this.hash.substr(1) + '"]');
+            if (target.length === 0) target = $('html');
+            $('html, body').animate({
+                scrollTop: target.offset().top - 75
+            }, 500);
+            return false;
+        });
 
-	$(window).scroll(function() {
-	    var height = $(window).scrollTop();
+        //SHOW FIXED NAV ON SCROLL
+        $(window).scroll(function() {
+            var scroll = $(window).scrollTop();
 
-	    if(height  >= 350) {
-	    	$('.nav ul').css('margin-top', '0px');
-	    } else if (height < 540) {
-	    	$('.nav ul').css('margin-top', '50px');
-	    }
-	});
+            if (scroll >= 350) {
+                $(".nav").addClass("switch");
+                $(".nav").addClass("logo");
+                $(".nav h3").removeClass("hide");
+                $(".nav ul").addClass("collapse");
+                $(".nav ul").addClass("space");
+            } else {
+                $(".nav").removeClass("switch");
+                $(".nav h3").addClass("hide");
+            }
+        });
 
+        var $mobileMenu = $('.mobile-menu');
+
+        //MOBILE MENU DISPLAY
+        $mobileMenu.click(function(e) {
+            e.preventDefault();
+            $('.nav ul').slideToggle('fast');
+        });
+
+
+        //HIDE MENU ON LINK CLICK
+        $(".nav li a").on('click', function() {
+            $(".nav-menu").hide();
+            $mobileMenu.toggleClass('open');
+        });
+
+        $(function() {
+            $mobileMenu.click(function(e) {
+                e.preventDefault();
+                $(this).toggleClass('open');
+            });
+        });
+
+        //RANDOM COLOUR GENERATOR FOR FOOTER SOCIAL ICONS
+        $('.social-footer a').on('mouseover', function() {
+            $(this).css('color', randomColor());
+        });
+
+        $('.social-footer a').on('mouseout', function() {
+            $(this).css('color', '#fff');
+        });
+
+
+    // })(jQuery);
 });
+
+// if($(window).width() <= 600){
+//   // do your stuff
+// }
